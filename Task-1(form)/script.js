4b7698be6b3334ac420eed5f57ac7c8382476a22
19dolly-hub <dollyraghuwanshi16@gmail.com>
2023-11-22 20:45:37 +0530

    optimize readability and maintainability

--- Task-1(form)/script.js
+++ Task-1(form)/script.js
@@ -1,17 +1,70 @@
+// GETTING THE ELEMENTS
 const nameField = document.getElementById("username");
-let isValidName = false;
-
 const emailField = document.getElementById("email");
-let isValidEmail = false;
-
 const pwField = document.getElementById("password");
-let isValidPw = false;
-
 const confirmField = document.getElementById("password-match");
+const togglePw = document.querySelector("icon");
+const submit = document.getElementById("submit");
+
+
+// INITIALIZE VALIDATION
+let isValidName = false;
+let isValidEmail = false;
+let isValidPw = false;
 let pwMatched = false;
 
-const togglePw = document.querySelector("icon");
 
+// ADDING EVENT-LISTENERS
+
+// CHECKING INPUT FIELDS
+nameField.addEventListener("change", e => {
+    const input = e.target.value;
+    const lettersExp = new RegExp("^[a-zA-Z]{3,}$");
+
+    isValidName = lettersExp.test(input);
+    updateState(nameField, isValidName);
+});
+
+emailField.addEventListener("change", e => {
+    const input = e.target.value;
+    const emailExp = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");   
+
+    isValidEmail = emailExp.test(input);
+    updateState(emailField, isValidEmail);
+});
+
+pwField.addEventListener("change", e => {
+    const input = e.target.value;
+    const pwExp = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})");
+
+    isValidPw = pwExp.test(input);
+    updateState(pwField, isValidPw);
+});
+
+confirmField.addEventListener("input", e => {
+    const input = e.target.value;
+
+    pwMatched = input === pwField.value;
+    updateState(confirmField, pwMatched);
+});
+
+
+// UPDATING STATE AS PER FIELD AND VALIDATION STATE
+function updateState(field, isValid) {
+    if(isValid) {
+        field.classList.remove("invalid");
+        field.classList.add("valid");
+        field.nextElementSibling.textContent = "";
+    }
+    else {
+        field.classList.remove("valid");
+        field.classList.add("invalid");
+        field.nextElementSibling.textContent = `Invalid ${field.id}`;
+    }
+}
+
+
+// TOGGLE PASSWORD VISIBILITY
 togglePw.addEventListener("click", () => {
     const type = confirmField.getAttribute("type") === "password" ? "text" : "password";
     confirmField.setAttribute("type", type);
@@ -20,77 +73,11 @@ togglePw.addEventListener("click", () => {
     document.getElementById("i-slash").classList.toggle("none");
 });
 
-const submit = document.getElementById("submit");
 
+// SUBMIT HANDLERS
 submit.addEventListener("click", e => {
     enableSubmit(e);
 });
-
-nameField.addEventListener("change", e => {
-    const input = e.target.value;
-    const lettersExp = new RegExp("^[a-zA-Z]{3,}$");
-
-    if(lettersExp.test(input)) {
-        isValidName = true;
-        validState(nameField);
-    }
-    else {
-        isValidName = false;
-        invalidState(nameField);
-    }
-});
-
-emailField.addEventListener("change", e => {
-    const input = e.target.value;
-    const emailExp = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");   
-
-    if(emailExp.test(input)) {
-        isValidEmail = true;
-        validState(emailField);
-    }
-    else {
-        isValidEmail = false;
-        invalidState(emailField);
-    }
-});
-
-pwField.addEventListener("change", e => {
-    const input = e.target.value;
-    const pwExp = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})");
-
-    if(pwExp.test(input)) {
-        isValidPw = true;
-        validState(pwField);
-    }
-    else {
-        isValidPw = false;
-        invalidState(pwField);
-    }
-});
-
-confirmField.addEventListener("input", e => {
-    const input = e.target.value;
-    if(input === pwField.value) {
-        pwMatched = true;
-        validState(confirmField);
-    }
-    else {
-        pwMatched = false;
-        invalidState(confirmField);
-    }
-});
-
-function validState(field) {
-    field.classList.remove("invalid");
-    field.classList.add("valid");
-    field.nextElementSibling.textContent = "";
-}
-
-function invalidState(field) {
-    field.classList.remove("valid");
-    field.classList.add("invalid");
-    field.nextElementSibling.textContent = `Invalid ${field.id}`;
-}
 
 function enableSubmit(e) {
     let allFieldsValid = isValidName && isValidEmail && isValidPw && pwMatched;
@@ -114,6 +101,6 @@ function enableSubmit(e) {
 function successSubmit(dataObject) {
     console.log(dataObject);
     document.querySelector("main").innerHTML = 
-    `<h1 style="color: white; text-transform: uppercase;">Thankyou! Data is printed on the console.</h1>`
+    `<h1 style="color: white; text-transform: uppercase;">Thankyou! Data has been printed on the console.</h1>`
     ;
 }
